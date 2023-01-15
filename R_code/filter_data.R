860691e93529fb506942f7d054bec1b30e561ee1
David Schwefel <ge47xec@tum.de>
2023-01-15 19:07:44 +0100

    first implementation of filter_data()

---
+++ R_code/filter_data.R
@@ -0,0 +1,46 @@
+
+
+library(tidyverse)
+data <- read_csv("C:/Users/HP/OneDrive/Desktop/EWS_projekt_Datensatz_1.csv")
+
+get_data <- function(saison_von,saison_bis=2022,teams="ALLTEAMS",ligen="ALLLEAGUES",platzierungen="ALLPLACES", punkte="ALLPOINTS",marktwert="ALLVALUES"){#TODO: use current year
+
+  #falschen/schlechten Input abfangen
+  if(typeof(saison_von)!="integer"|typeof(saison_bis)!="integer"|typeof(teams)!="character"|typeof(ligen)!="character"|
+    (platzierungen!="ALLPLACES"|typeof(platzierungen)!="integer")|(punkte!="ALLPOINTS"|typeof(punkte)!="integer")|(typeof(marktwert)!="integer"|marktwert!="ALLVALUES")){
+    return("Ein/Mehrere Paramter wurden falsch übergeben!")
+  }
+  
+  if(teams=="ALLTEAMS"&ligen=="ALLLEAGUES"){
+    return("FEHLER: Es muss mindestens ein Team oder eine Liga übergeben werden!")
+  }
+  
+  #Filtern
+  data %>%
+    filter(Saison>=saison_von,Saison<=saison_bis)
+  if(teams!="ALLTEAMS"){
+    data %>%
+      filter(Team %in% teams)
+  }
+  if(ligen!="ALLLEAGUES"){
+    data %>%
+      filter(Liga %in% ligen)
+  }
+  if(platzierungen!="ALLPLACES"){
+    data %>%
+      filter(Platzierung %in% platzierungen)
+  }
+  if(punkte!="ALLPOINTS"){
+    data %>%
+      filter(Punkte %in% punkte)
+  }
+  if(marktwert!="ALLVALUES"){
+    data %>%
+      filter(Marktwert %in% marktwert)
+  }
+}
+
+get_data(2021)
+
+
+
