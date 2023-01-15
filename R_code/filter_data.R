9215274ce3c6dd635cd2f185a7fa5071ed1a564e
David Schwefel <ge47xec@tum.de>
2023-01-15 19:23:39 +0100

    positionchnage of error-if-statements in filter_data()

--- R_code/filter_data.R
+++ R_code/filter_data.R
@@ -6,13 +6,13 @@ data <- read_csv("C:/Users/HP/OneDrive/Desktop/EWS_projekt_Datensatz_1.csv")
 get_data <- function(saison_von,saison_bis=2022,teams="ALLTEAMS",ligen="ALLLEAGUES",platzierungen="ALLPLACES", punkte="ALLPOINTS",marktwert="ALLVALUES"){#TODO: use current year
 
   #falschen/schlechten Input abfangen
+  if(teams=="ALLTEAMS"&ligen=="ALLLEAGUES"){
+    return("FEHLER: Es muss mindestens ein Team oder eine Liga übergeben werden!")
+  }
+  
   if(typeof(saison_von)!="integer"|typeof(saison_bis)!="integer"|typeof(teams)!="character"|typeof(ligen)!="character"|
     (platzierungen!="ALLPLACES"|typeof(platzierungen)!="integer")|(punkte!="ALLPOINTS"|typeof(punkte)!="integer")|(typeof(marktwert)!="integer"|marktwert!="ALLVALUES")){
     return("Ein/Mehrere Paramter wurden falsch übergeben!")
-  }
-  
-  if(teams=="ALLTEAMS"&ligen=="ALLLEAGUES"){
-    return("FEHLER: Es muss mindestens ein Team oder eine Liga übergeben werden!")
   }
   
   #Filtern
@@ -40,7 +40,7 @@ get_data <- function(saison_von,saison_bis=2022,teams="ALLTEAMS",ligen="ALLLEAGU
   }
 }
 
-get_data(2021)
+get_data(2021, team= "FC Augsburg",ligen=1)
 
 
 
