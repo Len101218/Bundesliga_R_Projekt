22cde323e9f161fbc1c8816bd48ef4a603efdadb
Len101218 <lh.bauer18@gmail.com>
2023-02-10 17:36:09 +0100

    .

--- Python/run.py
+++ Python/run.py
@@ -1,6 +1,8 @@
-from sys import argv
-from LoadPageModule  import *
+try:
+  from sys import argv
+  from LoadPageModule  import *
 
-if __name__ == '__main__':
+  if __name__ == '__main__':
     load(argv[1:])
-
+except ImportError as e:
+    pass
